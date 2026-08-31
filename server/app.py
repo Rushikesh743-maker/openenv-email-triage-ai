8eddc45a7fc968dcf86d7823cf416e53eba7fd13
Narke Rushikesh Ashruba <hrushikeshnarake@gmail.com>
2026-08-31 22:52:33 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,16 +1,3 @@
-from fastapi import FastAPI
-import uvicorn
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Server is running"}
-
-
-def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
 
 if __name__ == "__main__":
     main()
